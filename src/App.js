419e4d1a0f5b35d2536c5b35d6b8938545d60235
Sebastien Chagnon <sebastien.chagnon@polymtl.ca>
2020-04-19 19:36:08 -0400

    Added a filter to the employee List

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import React, { useState, Fragment } from 'react';
+import React, { useState, useCallback, Fragment } from 'react';
 import Employee from './Employee';
 import EmployeeInfo from './EmployeeInfo';
 
-import {Row, Col, Navbar, Container, Nav, ListGroup} from 'react-bootstrap';
+import {Row, Col, Navbar, Container, Nav, ListGroup, Card, Form} from 'react-bootstrap';
 
 const App = ({title}) => {
 
@@ -11,6 +11,23 @@ const App = ({title}) => {
     {employeeID: 1, firstName: "Pam", lastName: "Beesly", phoneNumber: "123-456-7890", cellphoneNumber: "012-345-6789"},
     {employeeID: 2, firstName: "Jim", lastName: "Halpert", phoneNumber: "123-456-7890", cellphoneNumber: "012-345-6789"},
   ]);
+
+  const [employeeFilter, setEmployeeFilter] = useState({
+    filter: "",
+    formerEmployees: false
+  });
+
+  const filterChangedHandle = useCallback(event => {
+    const newFilter = {...employeeFilter}
+    newFilter.filter = event.target.value;
+    setEmployeeFilter(newFilter);
+  }, [employeeFilter]);
+
+  const formerEmployeesChangedHandle = useCallback(event => {
+    const newFilter = {...employeeFilter}
+    newFilter.formerEmployees = event.target.checked;
+    setEmployeeFilter(newFilter);
+  }, [employeeFilter]);
 
   return (
     <Fragment>
@@ -27,15 +44,46 @@ const App = ({title}) => {
       <Container>
         <Row>
           <Col xs={12} sm={12} md={5} lg={4} xl={3}>
-            <ListGroup as="ul">
-              {
-                employees.map(employee => (
-                  <ListGroup.Item as="li">
-                    <Employee employeeID={employee.employeeID} firstName={employee.firstName} lastName={employee.lastName}/>
-                  </ListGroup.Item>
-                ))
-              }
-            </ListGroup>
+            <Row>
+              <Col>
+                <Card>
+                  <Card.Body>
+                    <Form>
+
+                      <Form.Group controlId="employeeFilter">
+                        <Form.Control type="text" placeholder="Filter" onChange={filterChangedHandle} value={employeeFilter.filter} />
+                      </Form.Group>
+
+                      <Form.Group controlId="formerEmployeeFilter" style={{marginBottom: 0}}>
+                        <Form.Check type="checkbox" label="Former Employees" onClick={formerEmployeesChangedHandle} value={employeeFilter.filter} />
+                      </Form.Group>
+
+                    </Form>
+                  </Card.Body>
+                </Card>
+              </Col>
+            </Row>
+            <Row>
+              <Col>
+                <ListGroup as="ul">
+                  {
+                    employees
+                      .filter(employee => {
+                        if (!employeeFilter.formerEmployees && employee.formerEmployee) return false;
+
+                        return employee.firstName.toLowerCase().includes(employeeFilter.filter.toLowerCase()) ||
+                          employee.lastName.toLowerCase().includes(employeeFilter.filter.toLowerCase()) ||
+                          employee.employeeID.toString().includes(employeeFilter.filter);
+                      })
+                      .map(employee => (
+                        <ListGroup.Item as="li">
+                          <Employee employeeID={employee.employeeID} firstName={employee.firstName} lastName={employee.lastName}/>
+                        </ListGroup.Item>
+                      ))
+                  }
+                </ListGroup>
+              </Col>
+            </Row>
           </Col>
           <Col>
             <EmployeeInfo {...employees[0]}/>
