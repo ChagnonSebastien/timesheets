e481f023c34a826e5f6d6e3146a2bd28e7757a78
Sebastien Chagnon <sebastien.chagnon@polymtl.ca>
2020-04-19 17:33:48 -0400

    Added Navbar

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import React, { useState, Fragment } from 'react';
 import Employee from './Employee';
 import EmployeeInfo from './EmployeeInfo';
 
-import {Row, Col} from 'react-bootstrap';
+import {Row, Col, Navbar, Container, Nav} from 'react-bootstrap';
 
-const App = () => {
+const App = ({title}) => {
 
   const [employees] = useState([
     {employeeID: 0, firstName: "Michael", lastName: "Scott", phoneNumber: "123-456-7890", cellphoneNumber: "012-345-6789"},
@@ -14,20 +14,52 @@ const App = () => {
 
   return (
     <Fragment>
-      <Row>
-        {
-          employees.map(employee => (
-            <Col>
-              <Employee id={employee.employeeID} firstName={employee.firstName} lastName={employee.lastName}/>
-            </Col>
-          ))
-        }
-      </Row>
-      <Row>
-        <Col>
-          <EmployeeInfo {...employees[0]}/>
-        </Col>
-      </Row>
+      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" sticky="top">
+        <Navbar.Brand href="#home">{title}</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link href="#employees">Employees</Nav.Link>
+            <Nav.Link href="#timesheets">Timesheets</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+      <Container>
+        <Row>
+          {
+            employees.map(employee => (
+              <Col>
+                <Employee id={employee.employeeID} firstName={employee.firstName} lastName={employee.lastName}/>
+              </Col>
+            ))
+          }
+        </Row>
+        <Row>
+          <Col>
+            <EmployeeInfo {...employees[0]}/>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <EmployeeInfo {...employees[0]}/>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <EmployeeInfo {...employees[0]}/>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <EmployeeInfo {...employees[1]}/>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <EmployeeInfo {...employees[2]}/>
+          </Col>
+        </Row>
+      </Container>
     </Fragment>
   )
 };
--- src/index.js
+++ src/index.js
@@ -2,14 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import Container from 'react-bootstrap/Container'
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Container>
-      <App />
-    </Container>
+    <App title="Company"/>
   </React.StrictMode>,
   document.getElementById('root')
 );
