9e6ca7c5151e0baeff28c8e0aedb32fd38ffe212
Sebastien Chagnon <sebastien.chagnon@polymtl.ca>
2020-04-19 16:20:29 -0400

    Added the employee list as a state of the App Component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Employee from './Employee';
 
-export default () => (
-  <div class="row">
-    <div class="col-12 col-sm-6 col-md-4 col-lg-3">
-      <Employee id={0} firstName={"Michael"} lastName={"Scott"}/>
+export default () => {
+
+  const [employees] = useState([
+    {employeeID: 0, firstName: "Michael", lastName: "Scott"},
+    {employeeID: 1, firstName: "Pam", lastName: "Beesly"},
+    {employeeID: 2, firstName: "Jim", lastName: "Halpert"},
+  ]);
+
+  return (
+    <div class="row">
+      {
+        employees.map(employee => (
+          <div class="col-12 col-sm-6 col-md-4 col-lg-3">
+            <Employee id={employee.employeeID} firstName={employee.firstName} lastName={employee.lastName}/>
+          </div>
+        ))
+      }
     </div>
-    <div class="col-12 col-sm-6 col-md-4 col-lg-3">
-      <Employee id={1} firstName={"Pam"} lastName={"Beesly"}/>
-    </div>
-    <div class="col-12 col-sm-6 col-md-4 col-lg-3">
-      <Employee id={2} firstName={"Jim"} lastName={"Halpert"}/>
-    </div>
-  </div>
-);
+  )
+};
