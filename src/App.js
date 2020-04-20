28f0751cb4a7cafe609a710b2b08979f23867976
Sebastien Chagnon <sebastien.chagnon@polymtl.ca>
2020-04-19 21:11:58 -0400

    Visual improvements in the app layout

--- src/App.js
+++ src/App.js
@@ -5,17 +5,19 @@ import EmployeeManagement from './EmployeeManagement';
 
 const App = ({title}) => (
   <Fragment>
-    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" sticky="top">
-      <Navbar.Brand href="#home">{title}</Navbar.Brand>
-      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-      <Navbar.Collapse id="responsive-navbar-nav">
-        <Nav className="mr-auto">
-          <Nav.Link href="#employees">Employees</Nav.Link>
-          <Nav.Link href="#timesheets">Timesheets</Nav.Link>
-        </Nav>
-      </Navbar.Collapse>
+    <Navbar collapseOnSelect expand="sm" bg="dark" variant="dark" sticky="top">
+      <Container>
+        <Navbar.Brand href="#home">{title}</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link href="#employees">Employees</Nav.Link>
+            <Nav.Link href="#timesheets">Timesheets</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
     </Navbar>
-    <Container>
+    <Container className='mt-3'>
       <EmployeeManagement />
     </Container>
   </Fragment>
--- src/EmployeeList.js
+++ src/EmployeeList.js
@@ -6,9 +6,23 @@ import EmployeeLabel from './EmployeeLabel';
 const EmployeeList = () => {
 
   const [employees] = useState([
-    {employeeID: 0, firstName: "Michael", lastName: "Scott", phoneNumber: "123-456-7890", cellphoneNumber: "012-345-6789"},
-    {employeeID: 1, firstName: "Pam", lastName: "Beesly", phoneNumber: "123-456-7890", cellphoneNumber: "012-345-6789"},
-    {employeeID: 2, firstName: "Jim", lastName: "Halpert", phoneNumber: "123-456-7890", cellphoneNumber: "012-345-6789"},
+    {employeeID: 0, firstName: "Michael", lastName: "Scott", formerEmployee: true},
+    {employeeID: 1, firstName: "Pam", lastName: "Beesly", formerEmployee: false},
+    {employeeID: 2, firstName: "Jim", lastName: "Halpert", formerEmployee: false},
+    {employeeID: 3, firstName: "Dwight", lastName: "Schrute", formerEmployee: false},
+    {employeeID: 4, firstName: "Kevin", lastName: "Malone", formerEmployee: false},
+    {employeeID: 5, firstName: "Stanley", lastName: "Hudson", formerEmployee: false},
+    {employeeID: 6, firstName: "Angela", lastName: "Martin", formerEmployee: false},
+    {employeeID: 7, firstName: "Phyllis", lastName: "Vance", formerEmployee: false},
+    {employeeID: 8, firstName: "Meredith", lastName: "Palmer", formerEmployee: false},
+    {employeeID: 9, firstName: "Creed", lastName: "Bratton", formerEmployee: false},
+    {employeeID: 10, firstName: "Oscar", lastName: "Martinez", formerEmployee: false},
+    {employeeID: 11, firstName: "Ryan", lastName: "Howard", formerEmployee: false},
+    {employeeID: 12, firstName: "Kelly", lastName: "Kapoor", formerEmployee: false},
+    {employeeID: 13, firstName: "Andy", lastName: "Bernard", formerEmployee: false},
+    {employeeID: 14, firstName: "Toby", lastName: "Flenderson", formerEmployee: false},
+    {employeeID: 15, firstName: "Darryl", lastName: "Philbin", formerEmployee: false},
+    {employeeID: 16, firstName: "Erin", lastName: "Hannon", formerEmployee: false},
   ]);
 
   const [employeeFilter, setEmployeeFilter] = useState({
@@ -30,7 +44,7 @@ const EmployeeList = () => {
 
   return (
     <Fragment>
-      <Row>
+      <Row className='mb-3'>
         <Col>
           <Card>
             <Card.Body>
@@ -51,7 +65,7 @@ const EmployeeList = () => {
       </Row>
       <Row>
         <Col>
-          <ListGroup as="ul">
+          <ListGroup as="ul" style={{height: '300px', overflowY: 'auto'}}>
             {
               employees
               .filter(employee => {
